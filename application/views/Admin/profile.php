e1dce47b735e5bdc62023ab2b44e9512cfab776d
Rizwan <rizwanhk29@gmail.com>
2023-03-31 19:25:55 +0530

    changes in profile

--- application/views/Admin/profile.php
+++ application/views/Admin/profile.php
@@ -1,3 +1,4 @@
+
 <?php include('include/header.php'); ?>  
 <div class="contents demo-card expanded">
    <div class="container-fluid">
@@ -38,6 +39,7 @@ $company = $this->common_model->GetSingleData('companies',array('admin_id'=>$emp
                      <input type="text" value="<?php echo $company['name']; ?>" name="company_name" class="form-control  ih-medium ip-lightradius-xs b-light" id="inputsummary" placeholder="Company Name">
                   </div>
                </div>
+               
                <div class="form-group col-sm-4">
                   <div class="with-icon">
                      <span class="la-user lar color-light"></span>
@@ -118,8 +120,124 @@ $company = $this->common_model->GetSingleData('companies',array('admin_id'=>$emp
       </div>
    </div>
 </div>
-<?php include('include/footer.php'); ?> 
+
+<!-- company -->
+<div class="contents demo-card expanded">
+   <div class="row">
+      <div class="col-sm-12">
+         <form method="post" action="<?php echo base_url(); ?>admin/profile" id="companySettingForm" enctype="multipart/form-data">
+             
+            <div id="eMsg"></div>
+            <h3>Company Settings</h3>
+            <div class="form-row">
+				
+				<div class="form-group col-sm-4">
+                  <div class="with-icon">
+                     <input type="text" value="" name="company_name" class="form-control  ih-medium ip-lightradius-xs b-light" id="inputsummary" placeholder="Company Name">
+                  </div>
+               </div>
+               <div class="form-group col-sm-4">
+                  <div class="with-icon">
+                     <textarea rows="4" cols="50" name="company_address" class="form-control  " id="inputsummary" placeholder="Company Address"></textarea>
+                  </div>
+               </div>
+               <div class="form-group col-sm-4">
+                  <div class="">
+                     <select name="states[]" id="statesSelect1"  class="form-control  ih-medium ip-lightradius-xs b-light " multiple >
+                     <?php foreach($states as $state) {
+                         ?>   
+                     <option value="<?php echo $state->state_id; ?>"><?php echo $state->state_name; ?></option>
+                       <?php }  ?>
+                     </select>
+                  </div>
+               </div>
+               <input type="hidden" name="company_setting" value="COMPUTER_SETTING">
+               <div class="form-group col-sm-4">
+                  <div class="with-icon">
+                     <select name="" id="time_zone"  class="form-control  ih-medium ip-lightradius-xs b-light" >
+                        <option value="">Choose a Time Zone</option>
+                     </select>
+                  </div>
+               </div>
+               <div class="form-group col-sm-4">
+                  <div class="with-icon">
+                     <input type="text" value="" name="pf_no" class="form-control  ih-medium ip-lightradius-xs b-light" id="inputsummary" placeholder="PF Number">
+                  </div>
+               </div>
+
+               <div class="form-group col-sm-4">
+                  <div class="with-icon">
+                     <input type="text" value="" name="tan_no" class="form-control  ih-medium ip-lightradius-xs b-light" id="inputsummary" placeholder="TAN Number">
+                  </div>
+               </div>
+
+               <div class="form-group col-sm-4">
+                  <div class="with-icon">
+                     <input type="text" value="" name="pan_no" class="form-control  ih-medium ip-lightradius-xs b-light" id="inputsummary" placeholder="PAN Number">
+                  </div>
+               </div>
+
+               <div class="form-group col-sm-4">
+                  <div class="with-icon">
+                     <input type="text" value="" name="esi_no" class="form-control  ih-medium ip-lightradius-xs b-light" id="inputsummary" placeholder="ESI Number">
+                  </div>
+               </div>
+
+               <div class="form-group col-sm-4">
+                  <div class="with-icon">
+                     <input type="text" value="" name="lin_no" class="form-control  ih-medium ip-lightradius-xs b-light" id="inputsummary" placeholder="LIN Number">
+                  </div>
+               </div>
+
+               <div class="form-group col-sm-4">
+                  <div class="with-icon">
+                     <input type="text" value="" name="gst_no" class="form-control  ih-medium ip-lightradius-xs b-light" id="inputsummary" placeholder="GST Number">
+                  </div>
+               </div>
+
+               <div class="form-group col-sm-4">
+                  <div class="with-icon">
+                     <input type="text" value="" name="registration_certificate_no" class="form-control  ih-medium ip-lightradius-xs b-light" id="inputsummary" placeholder="Registration Certificate Number">
+                  </div>
+               </div>
+
+               <div class="form-group col-sm-4">
+                  <div class="with-icon">
+                     <input type="text" value="" name="twitter_handle" class="form-control  ih-medium ip-lightradius-xs b-light" id="inputsummary" placeholder="Twitter Handle">
+                  </div>
+               </div>
+               
+             
+               <div class="form-group col-sm-4">
+                  <div class="with-icon">
+                     <!--<span class="la-user lar color-light"></span>-->
+                     <input type="file" name="company_logo">
+                     <img src="<?php echo base_url(); ?>assets/images/users/<?php echo $employees['profile_img']; ?>" style="height: 100px;width: 100px;">
+                  </div>
+               </div>
+              
+
+              <div class="form-group col-sm-12">
+                  <button type="submit" class="btn btn-primary btn-sm">Submit</button>
+               </div>
+               
+            </div>
+         </form>
+      </div>
+   </div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.4.slim.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+<?php //include('include/footer.php'); ?> 
 <script type="text/javascript">
+
+$(document).ready(function() {
+    $('#statesSelect').select2();
+});
+
    function addProfile(){
   // alert('hi');
    $.ajax({
@@ -146,6 +264,9 @@ $company = $this->common_model->GetSingleData('companies',array('admin_id'=>$emp
     });
    return false;  
 }  
+
+
+
 
 </script>
 <style>
