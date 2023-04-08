c9aa939d8046e1b95ea438ce34816477cc691fc1
rohit <rohitsaahu@gmail.com>
2023-04-08 19:22:31 +0530

    add fields

--- application/views/Admin/s_pf_leaving_reason.php
+++ application/views/Admin/s_pf_leaving_reason.php
@@ -1,6 +1,6 @@
 <?php include('include/header.php'); ?>  
 
- 
+
 <div class="contents demo-card expanded">
 	<div class="container-fluid">
 		<div class="row">
@@ -15,7 +15,7 @@
 								</span>
 							</li>
 							<li class="atbd-breadcrumb__item">
-								<span>Qualification</span>
+								<span>PF Leaving Reason</span>
 							</li>
 						</ul>
 					</div>
@@ -29,9 +29,9 @@
 			<div class="col-md-12">
 				<div class="card mb-30">
 					<div class="card-header">
-						<h5>Qualification List</h5>
+						<h5>PF Leaving Reason List</h5>
 						<div class="card-extra">
-							<button class="btn btn-sm btn-primary" data-toggle="modal" data-target="#Add_NewCompany">+ Add New Qualification</button>
+							<button class="btn btn-sm btn-primary" data-toggle="modal" data-target="#Add_NewCompany">+ Add New PF Leaving Reason</button>
 						</div>
 					</div>
 					<?php echo $this->session->flashdata('msg');
@@ -52,24 +52,24 @@
 									</tr>
 								</thead>
 								<tbody>
-								<?php $i = 1;if($qualification){ foreach($qualification as $key => $value){
+								<?php $i = 1;if($pf_leaving_reason){ foreach($pf_leaving_reason as $key => $value){
 									//$package = $this->common_model->GetSingleData('packages',array('id'=>$value['package_id']));
 									//$admins = $this->common_model->GetSingleData('users',array('user_id'=>$value['admin_id']));
 									?>
 									<tr>
 										<td><?php echo $i;?></td>
-										<td><?php echo $value['qualification'];?></td>
+										<td><?php echo $value['description'];?></td>
 										<td><?php echo date('d M Y',strtotime($value['created_date']));?></td>
 										<td><?php echo $value['status']=='1'?"<span class='badge badge-success rounded-pill'>Active":"<span class='badge badge-danger rounded-pill'>Inactive";?></span></td>
 										<td>
 											<div class="atbd-button-list d-flex flex-wrap">
 											   <button class="btn btn-icon btn-success btn-squared" title="Edit" data-toggle="modal" data-target="#Edit_NewCompany<?php echo $i; ?>"><i class="la la-edit mr-0"></i></button>
-												<a onclick="return confirm('Are you sure want to delete this qualification?');" href="<?php echo base_url(); ?>superadmin/deletequalification/<?php echo $value['id']; ?>"><button class="btn btn-icon btn-danger btn-squared" title="Delete" data-toggle="modal" data-target="#modal-delete"><i class="la la-trash mr-0"></i></button></a>
+												<a onclick="return confirm('Are you sure want to delete this PF Leaving Reason?');" href="<?php echo base_url(); ?>superadmin/deletepf_leaving_reason/<?php echo $value['id']; ?>"><button class="btn btn-icon btn-danger btn-squared" title="Delete" data-toggle="modal" data-target="#modal-delete"><i class="la la-trash mr-0"></i></button></a>
 									
 												<?php if($value['status']=='0'){?>
-												<a onclick="return confirm('Are you sure want to change status of this qualification?');" href="<?php echo base_url(); ?>superadmin/changeStatusqualification/<?php echo $value['id'];?>/1"><button class="btn btn-icon btn-danger btn-squared" title="Enable" ><i class="la la-ban mr-0"></i></button></a>
+												<a onclick="return confirm('Are you sure want to change status of this PF Leaving Reason?');" href="<?php echo base_url(); ?>superadmin/changeStatuspf_leaving_reason/<?php echo $value['id'];?>/1"><button class="btn btn-icon btn-danger btn-squared" title="Enable" ><i class="la la-ban mr-0"></i></button></a>
 												<?php }else{?>
-												<a onclick="return confirm('Are you sure want to change status of this qualification?');" href="<?php echo base_url(); ?>superadmin/changeStatusqualification/<?php echo $value['id'];?>/0"><button class="btn btn-icon btn-danger btn-squared" title="Disable" ><i class="la la-ban mr-0"></i></button></a>
+												<a onclick="return confirm('Are you sure want to change status of this PF Leaving Reason?');" href="<?php echo base_url(); ?>superadmin/changeStatuspf_leaving_reason/<?php echo $value['id'];?>/0"><button class="btn btn-icon btn-danger btn-squared" title="Disable" ><i class="la la-ban mr-0"></i></button></a>
 												<?php }?>
 												</div>
 										</td>
@@ -80,19 +80,19 @@
 									<div class="modal-dialog modal-md" role="document">
 										<div class="modal-content modal-bg-white ">
 											<div class="modal-header">
-												<h6 class="modal-title">Edit New Qualification</h6>
+												<h6 class="modal-title">Edit New PF Leaving Reason</h6>
 												<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 													<span data-feather="x"></span></button>
 											</div>
 											<div class="modal-body">
 												<div class="Vertical-form">
-													<form action="<?php echo base_url(); ?>superadmin/editqualification/<?php echo $value['id']; ?>" method="post" id="companyForm2" enctype="multipart/form-data">
+													<form action="<?php echo base_url(); ?>superadmin/editpf_leaving_reason/<?php echo $value['id']; ?>" method="post" id="companyForm2" enctype="multipart/form-data">
 														<div class="form-row">
 															
 															
 															<div class="form-group col-sm-12">
 																<label>Name</label>
-																<input type="text" name="name" value="<?php echo $value['qualification'];?>" class="form-control ih-medium ip-gray radius-xs b-light px-15"  placeholder="Enter Name">
+																<input type="text" name="pf_leaving_reason" value="<?php echo $value['description'];?>" class="form-control ih-medium ip-gray radius-xs b-light px-15"  placeholder="Enter Name">
 															</div>
 															
 															<div class="layout-button mt-25">
@@ -128,20 +128,20 @@
 <div class="modal-dialog modal-md" role="document">
 	<div class="modal-content modal-bg-white ">
 		<div class="modal-header">
-			<h6 class="modal-title">Add New Qualification</h6>
+			<h6 class="modal-title">Add New PF Leaving Reason</h6>
 			<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 				<span data-feather="x"></span></button>
 		</div>
 		<div class="modal-body">
 			<div class="Vertical-form">
-				<form action="<?php echo base_url(); ?>superadmin/addqualification" method="post" id="companyForm" enctype="multipart/form-data">
+				<form action="<?php echo base_url(); ?>superadmin/addpf_leaving_reason" method="post" id="companyForm" enctype="multipart/form-data">
 					<div class="form-row">
 						
 						
 						
 						<div class="form-group col-sm-12">
 							<label> Name</label>
-							<input type="text" name="name" class="form-control ih-medium ip-gray radius-xs b-light px-15"  placeholder="Enter Name">
+							<input type="text" name="pf_leaving_reason" class="form-control ih-medium ip-gray radius-xs b-light px-15"  placeholder="Enter Name">
 						</div>
 						
 						<div class="layout-button mt-25">
